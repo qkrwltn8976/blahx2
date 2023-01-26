10be10837e8d4eb68a99fd009f618d1fe2880fe0
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2023-01-26 20:26:23 +0900

    feat: firebase client 설정

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    // 서버 사이드 값을 클라이언트 사이드에 전달
+    apiKey: process.env.publicApiKey || '',
+    authDomain: process.env.FIREBASE_AUTH_HOST,
+    projectId: process.env.projectId || '',
+  },
+};
