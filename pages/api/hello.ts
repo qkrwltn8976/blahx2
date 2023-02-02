5f99771539063b3c014c0157cd7661b68447ee9f
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2023-02-02 20:34:00 +0900

    feat: 사용자 추가 API 수정

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,5 +5,5 @@ import FirebaseAdmin from '@/models/firebase_admin';
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
   FirebaseAdmin.getInstance().Firebase.collection('test');
-  res.status(200).json({ name: 'John doe' });
+  res.status(200).json({ name: 'John Doe' });
 }
